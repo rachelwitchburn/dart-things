9978b8cc7affc6c4f866f9a2920dd738d57f845b
rachelwitchburn <131322647+rachelwitchburn@users.noreply.github.com>
2023-09-26 17:18:30 -0300

    Add files via upload

---
+++ tananan.dart
@@ -0,0 +1,24 @@
+//primeiro teste dart
+void main() {
+  // criacao de variaveis
+  String nome = "Raquel";
+  print(nome);
+  int numero = 10;
+  print(numero);
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+  List<String> lista = ["Raquel", "Luiz"];
+  print('${lista[0]} - ${lista[1]}');
+
+  // null-safety
+
+  //IF e SWITCH
+
+  //metodos e classes
+
+  //future, async e await
+
+  // OO
+
+  //herança, polimorfismo e abstração
+}
