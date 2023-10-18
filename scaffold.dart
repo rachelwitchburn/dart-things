cb2d96d32cb2a9e45fddcdf55eada698e1b182b7
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2023-10-18 12:04:33 -0300

    Add files via upload

---
+++ scaffold.dart
@@ -0,0 +1,23 @@
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('Aplicativo Scaffold')),
+    ));
+  }
+}
+*/
